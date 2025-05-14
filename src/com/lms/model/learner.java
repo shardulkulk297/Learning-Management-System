b3d9c096ec08a1b333c2f07a188e45b476624e10
Shardul <shardulkulk297@gmail.com>
2025-05-14 10:34:39 +0530

    Started with the Project

---
+++ src/com/lms/model/learner.java
@@ -0,0 +1,42 @@
+package com.lms.model;
+
+public class learner {
+    private int id;
+    private String name;
+    private String email;
+
+    public learner(){
+
+    }
+    public learner(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "learner{" + "id=" + id + ", name=" + name + ", email=" + email + '}';
+    }
+
+}
